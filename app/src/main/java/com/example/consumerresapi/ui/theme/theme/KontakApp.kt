1e53371be61f120d36aed95721f5d5ece279ba74
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-12-27 22:49:41 +0700

    add icon

--- app/src/main/java/com/example/consumerresapi/ui/theme/theme/KontakApp.kt
+++ app/src/main/java/com/example/consumerresapi/ui/theme/theme/KontakApp.kt
@@ -2,8 +2,11 @@ package com.example.consumerresapi.ui.theme.theme
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -67,6 +70,10 @@ fun TopAppBarKontak(
     navigationIcon = {
         if (canNavigateBack) {
             IconButton(onClick = navigateUp) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = ""
+                )
 
             }
         }
