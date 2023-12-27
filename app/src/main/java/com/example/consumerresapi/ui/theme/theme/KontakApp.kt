8b1dedb96bf71ca780ed3d082d8ad004f9165c96
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-12-27 22:46:58 +0700

    add function TopAppBarKontak

--- app/src/main/java/com/example/consumerresapi/ui/theme/theme/KontakApp.kt
+++ app/src/main/java/com/example/consumerresapi/ui/theme/theme/KontakApp.kt
@@ -2,11 +2,8 @@ package com.example.consumerresapi.ui.theme.theme
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -64,4 +61,14 @@ fun TopAppBarKontak(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
-){}
+){CenterAlignedTopAppBar(title = { Text(title) },
+    modifier = modifier,
+    scrollBehavior = scrollBehavior,
+    navigationIcon = {
+        if (canNavigateBack) {
+            IconButton(onClick = navigateUp) {
+
+            }
+        }
+    })
+}
